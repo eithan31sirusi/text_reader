168d766f0e753e61e958df895d70d8c835d060e3
eithan31sirusi <eithan26sirusi@gmail.com>
2023-09-20 23:11:03 +0300

    Updates

---
+++ precache-manifest.64c5272e7a0c83ee2d4d60593d162f1a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b48d5a3de8a403fcab8",
+    "url": "/react-gh-pages/static/js/main.4b48d5a3.chunk.js"
+  },
+  {
+    "revision": "2a9f1eb4f5b1aab7233d",
+    "url": "/react-gh-pages/static/js/1.2a9f1eb4.chunk.js"
+  },
+  {
+    "revision": "346eb3eec4c1b18bf79d",
+    "url": "/react-gh-pages/static/js/runtime~main.346eb3ee.js"
+  },
+  {
+    "revision": "e4fdf487711602a52ec886e85f7710d6",
+    "url": "/react-gh-pages/index.html"
+  }
+];
